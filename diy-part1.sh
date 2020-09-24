e11c1676099bdab16051bd5ecbc185a003a4dd22
linyuhc001 <linyuhc001@gmail.com>
2020-09-24 21:25:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
