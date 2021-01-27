8be0bf9f64f71dabc02b9822e9e423f0f73c68f1
linyuhc001 <linyuhc001@gmail.com>
2021-01-27 11:16:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -155,6 +155,7 @@ CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
 # CONFIG_PACKAGE_luci-app-smartdns is not set #smartdns服务器
 # CONFIG_PACKAGE_luci-app-adguardhome is not set #ADguardhome
 # CONFIG_PACKAGE_luci-app-uugamebooster  is not set
+# CONFIG_PACKAGE_uugamebooster  is not set
 EOF
 
 # ShadowsocksR插件:
